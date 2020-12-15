86e22b479e2a187a267bf6a7e3d76e98e86cd9ee
daniel <danutdruta@gmail.com>
2020-12-16 00:34:17 +0200

    PartitionSchemeType enum

---
+++ src/main/java/ro/rainy/jusbootable/model/domain/PartitionSchemeType.java
@@ -0,0 +1,11 @@
+package ro.rainy.jusbootable.model.domain;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 16/12/2020__00:00
+ */
+public enum PartitionSchemeType {
+    MBR,
+    GPT;
+}
