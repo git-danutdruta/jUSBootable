803c5cf98557349632e034a6c2841e112ce8e715
daniel <danutdruta@gmail.com>
2020-12-15 23:39:26 +0200

    structure miss

---
+++ src/main/java/ro/rainy/jusbootable/view/Structure.java
@@ -0,0 +1,14 @@
+package ro.rainy.jusbootable.view;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 14/12/2020__19:06
+ */
+public interface Structure {
+    void init();
+
+    void setUp();
+
+    void build();
+}
