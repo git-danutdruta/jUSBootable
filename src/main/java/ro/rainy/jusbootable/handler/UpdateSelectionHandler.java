22d82b307c030317be1a4094e07d909dba13cf85
daniel <danutdruta@gmail.com>
2020-12-17 22:19:17 +0200

    UpdateSelectionHandler on boot selection text field

---
+++ src/main/java/ro/rainy/jusbootable/handler/UpdateSelectionHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.jusbootable.handler;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 17/12/2020__21:56
+ */
+public interface UpdateSelectionHandler {
+    void updateSelection(String fileName);
+}
