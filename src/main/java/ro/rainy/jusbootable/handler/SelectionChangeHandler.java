22b396e7b83f0b8ec740a08f8d09750c409a34f2
daniel <danutdruta@gmail.com>
2020-12-17 21:04:30 +0200

    new changes into jUSBView

---
+++ src/main/java/ro/rainy/jusbootable/handler/SelectionChangeHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.jusbootable.handler;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 16/12/2020__22:48
+ */
+public interface SelectionChangeHandler {
+    void selectionChange();
+}
--- src/main/java/ro/rainy/jusbootable/view/JUSBView.java
+++ src/main/java/ro/rainy/jusbootable/view/JUSBView.java
@@ -1,8 +1,11 @@
 package ro.rainy.jusbootable.view;
 
 import ro.rainy.jusbootable.handler.ButtonClickHandler;
-import ro.rainy.jusbootable.model.USBComboModel;
-import ro.rainy.jusbootable.model.domain.FlashDrive;
+import ro.rainy.jusbootable.handler.SelectionChangeHandler;
+import ro.rainy.jusbootable.model.BComboModel;
+import ro.rainy.jusbootable.model.domain.*;
+
+import java.awt.*;
 
 /**
  * @proiect: jUSBootable
@@ -12,11 +15,24 @@ import ro.rainy.jusbootable.model.domain.FlashDrive;
 public interface JUSBView extends Structure {
     void setVisible(boolean visible);
 
+    void setIconImage(Image image);
 
-    void setUSBComboBoxModel(USBComboModel<FlashDrive> comboBoxModel);
+    void setUSBComboBoxModel(BComboModel<FlashDrive> usbComboBoxModel);
+
+    void setPartitionSchemeComboBoxModel(BComboModel<PartitionSchemeType> partitionSchemeTypeComboModel);
+
+    void setTargetSystemComboBoxModel(BComboModel<TargetSystemType> targetSystemTypeComboModel);
+
+    void setFileSystemComboBoxModel(BComboModel<FileSystemType> fileSystemComboBoxModel);
+
+    void setClusterComboBoxModel(BComboModel<ClusterSize> fileSystemComboBoxModel);
 
 
     void whenUserClickStartButton(ButtonClickHandler startHandler);
 
     void whenUserClickCloseButton(ButtonClickHandler closeHandler);
+
+    void whenUserClickBootSelectionButton(ButtonClickHandler bootSelectionHandler);
+
+    void whenFileChooserSelectionChange(SelectionChangeHandler selectionChangeHandler);
 }
