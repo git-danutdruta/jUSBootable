b3b1df85b0cca62d693b3666d515fa1a8cf22350
daniel <danutdruta@gmail.com>
2020-12-18 08:19:55 +0200

    Exception interface handler

---
+++ src/main/java/ro/rainy/jusbootable/handler/ExceptionThrownHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.jusbootable.handler;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 12/12/2020__12:42
+ */
+public interface ExceptionThrownHandler {
+    void exceptionThrown(Throwable throwable);
+}
