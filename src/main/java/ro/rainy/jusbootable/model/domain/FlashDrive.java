f14a90ac98afc1c3b7763d08e4a1741595abe239
daniel <danutdruta@gmail.com>
2020-12-19 13:15:53 +0200

    add constructor in FlashDrive as adapter/mapper for USBStorageDevice

--- src/main/java/ro/rainy/jusbootable/model/domain/FlashDrive.java
+++ src/main/java/ro/rainy/jusbootable/model/domain/FlashDrive.java
@@ -1,17 +1,32 @@
 package ro.rainy.jusbootable.model.domain;
 
+import net.samuelcampos.usbdrivedetector.USBStorageDevice;
+
 /**
  * @proiect: jUSBootable
  * @autor: daniel
  * @data: 14/12/2020__14:35
  */
 public class FlashDrive {
-    private String name;
+    private final String deviceName;
+    private final String device;
+    private final String uuid;
+
+    public FlashDrive(String deviceName, String device, String uuid) {
+        this.deviceName = deviceName;
+        this.device = device;
+        this.uuid = uuid;
+    }
+
+
+    public FlashDrive(USBStorageDevice usbDevice) {
+        this.deviceName = usbDevice.getDeviceName();
+        this.device = usbDevice.getDevice();
+        this.uuid = usbDevice.getUuid();
+    }
 
     @Override
     public String toString() {
-        return "FlashDrive{" +
-                "name='" + name + '\'' +
-                '}';
+        return deviceName;
     }
 }
