a73f2534e680e20101f577c6e6c0fe528ddbbf36
daniel <danutdruta@gmail.com>
2020-12-19 13:13:30 +0200

    proper progressbar model

---
+++ src/main/java/ro/rainy/jusbootable/model/impl/BProgressBarBoundedRangeModelImpl.java
@@ -0,0 +1,13 @@
+package ro.rainy.jusbootable.model.impl;
+
+import ro.rainy.jusbootable.model.BProgressBarBoundedRangeModel;
+
+import javax.swing.*;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 18/12/2020__19:02
+ */
+public class BProgressBarBoundedRangeModelImpl extends DefaultBoundedRangeModel implements BProgressBarBoundedRangeModel {
+}
