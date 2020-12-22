2cb8051cd6db85a2a1bf3415139b2f4ff435484b
daniel <danutdruta@gmail.com>
2020-12-22 22:13:51 +0200

    igniter

--- src/main/java/ro/rainy/jusbootable/App.java
+++ src/main/java/ro/rainy/jusbootable/App.java
@@ -1,9 +1,29 @@
 package ro.rainy.jusbootable;
 
-/**
- * Hello world!
- */
+import ro.rainy.jusbootable.controller.JUSBController;
+import ro.rainy.jusbootable.model.JUSBModel;
+import ro.rainy.jusbootable.model.impl.JUSBModelImpl;
+import ro.rainy.jusbootable.view.JUSBView;
+import ro.rainy.jusbootable.view.impl.JUSBViewImpl;
+
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 public class App {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(NimbusLookAndFeel.class.getName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        ApplicationStarter starter = new ApplicationStarterImpl();
+
+        JUSBView view = new JUSBViewImpl("<:: jUSBootable ::>");
+        JUSBModel model = new JUSBModelImpl();
+        JUSBController controller = new JUSBController(view, model);
+
+        JUSBCoordinator coordinator = new JUSBCoordinator(starter, model);
+
+        starter.start();
     }
 }
