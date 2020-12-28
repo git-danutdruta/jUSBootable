039ec273aa42c1adcc9937fb56ea196da7cdcae6
daniel <danutdruta@gmail.com>
2020-12-28 23:38:02 +0200

    missing dep

---
+++ src/main/java/ro/rainy/jusbootable/handler/InfoDataHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.jusbootable.handler;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 21/12/2020__22:48
+ */
+public interface InfoDataHandler {
+    void pushDataToGUI(String message);
+}
