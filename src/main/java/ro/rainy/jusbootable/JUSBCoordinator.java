4070522f19eba1e9bd1016a9882daed91398e81f
daniel <danutdruta@gmail.com>
2020-12-29 22:29:33 +0200

    some logs

--- src/main/java/ro/rainy/jusbootable/JUSBCoordinator.java
+++ src/main/java/ro/rainy/jusbootable/JUSBCoordinator.java
@@ -1,5 +1,7 @@
 package ro.rainy.jusbootable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ro.rainy.jusbootable.model.JUSBModel;
 
 /**
@@ -8,9 +10,12 @@ import ro.rainy.jusbootable.model.JUSBModel;
  * @data: 14/12/2020__19:17
  */
 public class JUSBCoordinator {
+    private static final Logger LOG = LoggerFactory.getLogger(JUSBCoordinator.class);
+
     public JUSBCoordinator(ApplicationStarter applicationStarter, JUSBModel jUsbModel) {
 
         applicationStarter.whenApplicationStart(() -> {
+            LOG.info("Starting jUSBootable application");
             jUsbModel.setVisible(true);
             jUsbModel.checkDeviceListForUpdates();
         });
