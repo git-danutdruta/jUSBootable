aab920d5138d75c32c0289a7287ffef355a32162
daniel <danutdruta@gmail.com>
2020-12-31 18:45:51 +0200

    model test

---
+++ src/test/java/ro/rainy/jusbootable/model/JUSBModelTest.java
@@ -0,0 +1,23 @@
+package ro.rainy.jusbootable.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import ro.rainy.jusbootable.ContextHolder;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 31/12/2020__18:44
+ */
+public class JUSBModelTest extends ContextHolder {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
