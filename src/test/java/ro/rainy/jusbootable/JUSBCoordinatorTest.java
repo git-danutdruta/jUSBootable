a0d67f10d41c63652f1658d47c610395085e2ead
daniel <danutdruta@gmail.com>
2020-12-31 18:43:10 +0200

    coordinator adv test

--- src/test/java/ro/rainy/jusbootable/JUSBCoordinatorTest.java
+++ src/test/java/ro/rainy/jusbootable/JUSBCoordinatorTest.java
@@ -1,9 +1,20 @@
 package ro.rainy.jusbootable;
 
+import org.junit.Before;
+
 /**
  * @proiect: jUSBootable
  * @autor: daniel
  * @data: 31/12/2020__18:40
  */
-public class JUSBCoordinatorTest {
+public class JUSBCoordinatorTest extends ContextHolder{
+
+    private ApplicationStarter applicationStarter;
+
+    @Before
+    public void setUp(){
+        applicationStarter = context.mock(ApplicationStarter.class);
+    }
+
+
 }
