df8eea6f8beae9457a59d67868845d4adb51aae3
daniel <danutdruta@gmail.com>
2021-01-01 19:08:25 +0200

    some expectations in controller setup test

--- src/test/java/ro/rainy/jusbootable/controller/JUSBControllerTest.java
+++ src/test/java/ro/rainy/jusbootable/controller/JUSBControllerTest.java
@@ -1,9 +1,12 @@
 package ro.rainy.jusbootable.controller;
 
+import org.jmock.Expectations;
 import org.junit.Before;
 import org.junit.Test;
 import ro.rainy.jusbootable.ContextHolder;
+import ro.rainy.jusbootable.InstanceCatcher;
 import ro.rainy.jusbootable.handler.ExceptionThrownHandler;
+import ro.rainy.jusbootable.handler.InfoDataHandler;
 import ro.rainy.jusbootable.handler.VisibilityChangeHandler;
 import ro.rainy.jusbootable.model.JUSBModel;
 import ro.rainy.jusbootable.view.JUSBView;
@@ -17,17 +20,32 @@ public class JUSBControllerTest extends ContextHolder {
     private JUSBView view;
     private JUSBModel model;
 
-    private VisibilityChangeHandler visibilityChangeHandler;
-    private ExceptionThrownHandler exceptionThrownHandler;
+    private InstanceCatcher<VisibilityChangeHandler> visibilityChangeHandlerCatcher;
+    private InstanceCatcher<ExceptionThrownHandler> exceptionThrownHandlerInstanceCatcher;
+    private InstanceCatcher<InfoDataHandler> infoDataHandlerInstanceCatcher;
 
     @Before
     public void setUp() {
         model = context.mock(JUSBModel.class);
         view = context.mock(JUSBView.class);
+
+        visibilityChangeHandlerCatcher = new InstanceCatcher<>();
+        exceptionThrownHandlerInstanceCatcher = new InstanceCatcher<>();
+        infoDataHandlerInstanceCatcher = new InstanceCatcher<>();
+
+        context.checking(new Expectations() {
+            {
+                oneOf(model).whenExceptionThrown(with(exceptionThrownHandlerInstanceCatcher));
+                oneOf(model).whenInfoSend(with(infoDataHandlerInstanceCatcher));
+                oneOf(model).whenVisibilityChange(with(visibilityChangeHandlerCatcher));
+            }
+        });
+
+//        new JUSBController(view, model);
     }
 
     @Test
     public void test() {
-
     }
+
 }
