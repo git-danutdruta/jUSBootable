f0429bb5ef03574896c175d9e1f895b130859d6a
daniel <danutdruta@gmail.com>
2021-01-02 19:21:35 +0200

    visibleTest method

--- src/test/java/ro/rainy/jusbootable/view/JUSBViewTest.java
+++ src/test/java/ro/rainy/jusbootable/view/JUSBViewTest.java
@@ -1,5 +1,8 @@
 package ro.rainy.jusbootable.view;
 
+import org.jmock.Expectations;
+import org.junit.Before;
+import org.junit.Test;
 import ro.rainy.jusbootable.ContextHolder;
 
 /**
@@ -8,5 +11,18 @@ import ro.rainy.jusbootable.ContextHolder;
  * @data: 02/01/2021__19:17
  */
 public class JUSBViewTest extends ContextHolder {
+    private JUSBView view;
 
+    @Before
+    public void setUp() {
+        view = context.mock(JUSBView.class);
+    }
+
+    @Test
+    public void setVisibilityOnViewTest() {
+        context.checking(new Expectations() {{
+            oneOf(view).setVisible(true);
+            oneOf(view).setVisible(false);
+        }});
+    }
 }
