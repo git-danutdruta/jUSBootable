0b67dbd0a7d8af547b2e66d7102175eef5351886
daniel <danutdruta@gmail.com>
2021-01-02 19:17:37 +0200

    view test class

---
+++ src/test/java/ro/rainy/jusbootable/view/JUSBViewTest.java
@@ -0,0 +1,12 @@
+package ro.rainy.jusbootable.view;
+
+import ro.rainy.jusbootable.ContextHolder;
+
+/**
+ * @proiect: jUSBootable
+ * @autor: daniel
+ * @data: 02/01/2021__19:17
+ */
+public class JUSBViewTest extends ContextHolder {
+
+}
