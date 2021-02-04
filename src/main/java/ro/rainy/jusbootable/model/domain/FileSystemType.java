cab0e513d32308c8025ab4d845f225910fecd800
danutdruta <danutdruta@gmail.com>
2021-02-04 12:20:39 +0200

    determine OS & group fileTypes by Operating System

--- src/main/java/ro/rainy/jusbootable/model/domain/FileSystemType.java
+++ src/main/java/ro/rainy/jusbootable/model/domain/FileSystemType.java
@@ -1,5 +1,11 @@
 package ro.rainy.jusbootable.model.domain;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @proiect: jUSBootable
  * @autor: daniel
@@ -9,7 +15,7 @@ public enum FileSystemType {
     FAT32(OperatingSystem.WINDOWS, "", "FAT32"),
     NTFS_DOS(OperatingSystem.WINDOWS, "", "NTFS"),
 
-    EX_FAT_NIX(OperatingSystem.LINUX, "mkfs.exfat", "EX_FAT_NIX"),
+    EX_FAT_NIX(OperatingSystem.LINUX, "mkfs.exfat", "EX_FAT"),
     FAT_NIX(OperatingSystem.LINUX, "mkfs.fat", "FAT"),
     MSDOS_NIX(OperatingSystem.LINUX, "mkfs.msdos", "MSDOS"),
     V_FAT_NIX(OperatingSystem.LINUX, "mkfs.vfat", "V_FAT"),
@@ -24,6 +30,37 @@ public enum FileSystemType {
         this.os = os;
         this.utilityProcess = utilityProcess;
         this.description = description;
+    }
+
+
+    public static Set<FileSystemType> getFileSystemTypes() {
+        return getFileSystemTypes_().get(getCurrentOS());
+    }
+
+    private static Map<OperatingSystem, Set<FileSystemType>> getFileSystemTypes_() {
+        return Stream
+                .of(FileSystemType.values())
+                .collect(Collectors.groupingBy(FileSystemType::getOs, Collectors.toSet()));
+    }
+
+    private static OperatingSystem getCurrentOS() {
+        OperatingSystem os = null;
+        String operSys = System.getProperty("os.name").toLowerCase();
+        if (operSys.contains("win")) {
+            os = OperatingSystem.WINDOWS;
+        } else if (operSys.contains("nix") || operSys.contains("nux")
+                || operSys.contains("aix")) {
+            os = OperatingSystem.LINUX;
+        } else if (operSys.contains("mac")) {
+            os = OperatingSystem.MAC_OS;
+            /*} else if (operSys.contains("sunos")) {
+                os = OS.SOLARIS;*/
+        }
+        return os;
+    }
+
+    public OperatingSystem getOs() {
+        return os;
     }
 
     @Override
