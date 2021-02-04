5d72e43341d03c793cfb502d5d4f2b4e39c3796c
danutdruta <danutdruta@gmail.com>
2021-02-04 11:59:51 +0200

    mark formatting fileSystemType by utility program depending on OS

--- src/main/java/ro/rainy/jusbootable/model/domain/FileSystemType.java
+++ src/main/java/ro/rainy/jusbootable/model/domain/FileSystemType.java
@@ -6,13 +6,23 @@ package ro.rainy.jusbootable.model.domain;
  * @data: 15/12/2020__23:57
  */
 public enum FileSystemType {
-    FAT32("FAT32"),
-    EX_FAT("exFAT"),
-    NTFS("NTFS");
+    FAT32(OperatingSystem.WINDOWS, "", "FAT32"),
+    NTFS_DOS(OperatingSystem.WINDOWS, "", "NTFS"),
+
+    EX_FAT_NIX(OperatingSystem.LINUX, "mkfs.exfat", "EX_FAT_NIX"),
+    FAT_NIX(OperatingSystem.LINUX, "mkfs.fat", "FAT"),
+    MSDOS_NIX(OperatingSystem.LINUX, "mkfs.msdos", "MSDOS"),
+    V_FAT_NIX(OperatingSystem.LINUX, "mkfs.vfat", "V_FAT"),
+    NTFS_NIX(OperatingSystem.LINUX, "mkfs.ntfs", "NTFS");
 
     private String description;
+    private OperatingSystem os;
+    private String utilityProcess;
 
-    FileSystemType(String description) {
+
+    FileSystemType(OperatingSystem os, String utilityProcess, String description) {
+        this.os = os;
+        this.utilityProcess = utilityProcess;
         this.description = description;
     }
 
