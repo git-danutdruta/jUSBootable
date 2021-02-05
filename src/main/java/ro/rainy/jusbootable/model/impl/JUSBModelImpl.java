917f44930340fe48c9627a15edc83485f2e73a10
danutdruta <danutdruta@gmail.com>
2021-02-05 12:40:58 +0200

    organize import

--- src/main/java/ro/rainy/jusbootable/model/impl/JUSBModelImpl.java
+++ src/main/java/ro/rainy/jusbootable/model/impl/JUSBModelImpl.java
@@ -21,7 +21,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @proiect: jUSBootable
