743bdb1acc02da495f96d572592e628845e5420d
danutdruta <danutdruta@gmail.com>
2021-02-07 14:56:11 +0200

    interacting data handler

---
+++ src/main/java/ro/rainy/jusbootable/handler/InteractingDataHandler.java
@@ -0,0 +1,10 @@
+package ro.rainy.jusbootable.handler;
+
+/**
+ * @project: jUSBootable
+ * @author: daniel
+ * @time: 06/02/2021  19:56
+ */
+public interface InteractingDataHandler {
+    void interactMessage(String message);
+}
