867c2a1dd45530c07eb03be7ef1bd740b98b6313
danutdruta <danutdruta@gmail.com>
2021-02-08 15:53:46 +0200

    TextDocument inherit DocumentListener

--- src/main/java/ro/rainy/jusbootable/model/BTextDocumentModel.java
+++ src/main/java/ro/rainy/jusbootable/model/BTextDocumentModel.java
@@ -1,10 +1,12 @@
 package ro.rainy.jusbootable.model;
 
+import javax.swing.event.DocumentListener;
+
 /**
  * @project: jUSBootable
  * @author: daniel
  * @time: 08/02/2021  21:48
  */
-public interface BTextDocumentModel/* extends DocumentListener*/ {
+public interface BTextDocumentModel extends DocumentListener {
 
 }
