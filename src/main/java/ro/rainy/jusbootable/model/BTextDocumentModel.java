0e9b28f1bd9501921342587260e3bc062d1ce259
danutdruta <danutdruta@gmail.com>
2021-02-08 15:53:00 +0200

    TextDocumentModel definition

---
+++ src/main/java/ro/rainy/jusbootable/model/BTextDocumentModel.java
@@ -0,0 +1,10 @@
+package ro.rainy.jusbootable.model;
+
+/**
+ * @project: jUSBootable
+ * @author: daniel
+ * @time: 08/02/2021  21:48
+ */
+public interface BTextDocumentModel/* extends DocumentListener*/ {
+
+}
